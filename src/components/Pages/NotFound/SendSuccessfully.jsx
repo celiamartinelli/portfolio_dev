0c9072a380f0335c55dbf45513a66945c9171a10
celiamartinelli <celia.martinelli@oclock.school>
2024-09-12 12:26:53 +0200

    add

--- src/components/Pages/NotFound/SendSuccessfully.jsx
+++ src/components/Pages/NotFound/SendSuccessfully.jsx
@@ -4,14 +4,14 @@ export default function SendSuccessfully({ children, onClose }) {
   const { t } = useTranslation();
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white dark:bg-darkPruneBG p-6 rounded-lg shadow-lg">
+      <div className="bg-white dark: p-6 rounded-lg shadow-lg">
         {children}
         <button
           type="button"
           onClick={onClose}
-          className="mt-4 bg-vertFonce1 dark:bg-darkBlue text-white p-2 rounded w-full flex justify-center items-center active:bg-emerald-700 transition ease-in duration-25 transform hover:scale-95 shadow-md"
+          className="mt-4 bg-vertFonce1 dark:bg-darkBlue text-black dark:text-white p-2 rounded w-full flex justify-center items-center active:bg-emerald-700 transition ease-in duration-25 transform hover:scale-95 shadow-md"
         >
-          {t('modal.close')}
+          {t('form.close')}
         </button>
       </div>
     </div>
