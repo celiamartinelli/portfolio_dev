ba9dc665ccb2313d291e23ff6e7e4439774cf615
celiamartinelli <celia.martinelli@oclock.school>
2024-09-12 09:35:58 +0200

    dd

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/project/:path" element={<ProjectPage />} />
-          <Route path="*" element={<NotFoundPage />} />
+          <Route path="/*" element={<NotFoundPage />} />
         </Routes>
       </div>
     </DarkModeProvider>
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 export default function Form() {
@@ -7,62 +8,79 @@ export default function Form() {
   const [reason, setReason] = useState('');
   const [description, setDescription] = useState('');
   const [email, setEmail] = useState('');
+  const location = useLocation();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
 
-    console.log({ name, reason, description, email });
-  };
+  //   console.log({ name, reason, description, email });
+  // };
+
+  const formRef = useRef < HTMLFormElement > null;
+
+  useEffect(() => {
+    if (location.state?.scrollToContact) {
+      const element = document.getElementById('contact-form');
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, [location]);
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="flex flex-col rounded-lg p-4 bg-lightMint dark:bg-darkBG dark:bg-opacity-85 w-80 h-150 my-5 shadow-lg"
-      name="contact"
-      method="POST"
-      data-netlify="true"
-    >
-      <label htmlFor="name" className="mb-2 text-white">
-        {t('form.name')}
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="border mt-1 p-2 rounded bg-white text-black w-full focus:outline-none focus:ring-2 focus:ring-emerald-500 shadow-inner shadow-emerald-500 dark:focus:ring-rose-500 dark:shadow-rose-400"
-        />
-      </label>
-      <label htmlFor="reason" className="mb-2 text-white">
-        {t('form.subject')}
-        <input
-          type="text"
-          value={reason}
-          onChange={(e) => setReason(e.target.value)}
-          className="border mt-1 p-2 rounded bg-white text-black w-full focus:outline-none focus:ring-2 focus:ring-emerald-500 shadow-inner shadow-emerald-500 dark:focus:ring-rose-500 dark:shadow-rose-400"
-        />
-      </label>
-      <label htmlFor="description" className="mb-2 text-white">
-        {t('form.message')}
-        <textarea
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          className="border mt-1 p-2 rounded bg-white text-black w-full focus:outline-none border-none focus:ring-2 focus:ring-emerald-500 shadow-inner shadow-emerald-500 dark:focus:ring-rose-500 dark:shadow-rose-400"
-        />
-      </label>
-      <label htmlFor="email" className="mb-2 text-white">
-        {t('form.email')}
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="border mt-1 p-2 rounded bg-white text-black w-full focus:outline-none focus:ring-2 focus:ring-emerald-500 shadow-inner shadow-emerald-500 dark:focus:ring-rose-500 dark:shadow-rose-400"
-        />
-      </label>
-      <button
-        type="submit"
-        className="mt-4 bg-vertFonce1 dark:bg-darkBlue text-white p-2 rounded w-full flex justify-center items-center active:bg-emerald-700 transition ease-in duration-25 transform hover:scale-95 shadow-md"
+    <div id="contact-form">
+      <form
+        // onSubmit={handleSubmit}
+        className="flex flex-col rounded-lg p-4 bg-lightMint dark:bg-darkBG dark:bg-opacity-85 w-80 h-150 my-5 shadow-lg"
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        action="/"
+        ref={formRef}
       >
-        {t('form.send')}
-      </button>
-    </form>
+        <label htmlFor="name" className="mb-2 text-white">
+          {t('form.name')}
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="border mt-1 p-2 rounded bg-white text-black w-full focus:outline-none focus:ring-2 focus:ring-emerald-500 shadow-inner shadow-emerald-500 dark:focus:ring-rose-500 dark:shadow-rose-400"
+          />
+        </label>
+        <label htmlFor="reason" className="mb-2 text-white">
+          {t('form.subject')}
+          <input
+            type="text"
+            value={reason}
+            onChange={(e) => setReason(e.target.value)}
+            className="border mt-1 p-2 rounded bg-white text-black w-full focus:outline-none focus:ring-2 focus:ring-emerald-500 shadow-inner shadow-emerald-500 dark:focus:ring-rose-500 dark:shadow-rose-400"
+          />
+        </label>
+        <label htmlFor="description" className="mb-2 text-white">
+          {t('form.message')}
+          <textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            className="border mt-1 p-2 rounded bg-white text-black w-full focus:outline-none border-none focus:ring-2 focus:ring-emerald-500 shadow-inner shadow-emerald-500 dark:focus:ring-rose-500 dark:shadow-rose-400"
+          />
+        </label>
+        <label htmlFor="email" className="mb-2 text-white">
+          {t('form.email')}
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="border mt-1 p-2 rounded bg-white text-black w-full focus:outline-none focus:ring-2 focus:ring-emerald-500 shadow-inner shadow-emerald-500 dark:focus:ring-rose-500 dark:shadow-rose-400"
+          />
+        </label>
+        <button
+          type="submit"
+          className="mt-4 bg-vertFonce1 dark:bg-darkBlue text-white p-2 rounded w-full flex justify-center items-center active:bg-emerald-700 transition ease-in duration-25 transform hover:scale-95 shadow-md"
+        >
+          {t('form.send')}
+        </button>
+      </form>
+    </div>
   );
 }
